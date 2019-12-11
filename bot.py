b1ef8efac6448cdd46e891262478f5236d77e3bf
tye-beach <55841503+tye-beach@users.noreply.github.com>
2019-12-10 19:15:19 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,4 +3,4 @@ import discord
 
 bot = commands.Bot(command_prefix="!", status=discord.Status.idle, activity=discord.Game(name="Booting.."))
 
-bot.run("NjU0MTA2NjI2MTM1MDMxODA4.XfAuhg.XsVLmbg74iZF2t-VZJGH4Z4Xvgc")
+bot.run("NjU0MTA2NjI2MTM1MDMxODA4.XfA07Q.BV3xD7t9jYjGm1eiV54VLNlML2U")
